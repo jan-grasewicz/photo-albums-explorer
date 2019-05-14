1294225aca3fe7830144f78fbb1a1c2b495da59a
jan-grasewicz <j.grasewicz@gmail.com>
2019-05-14 22:30:50 +0200

    thumbnails added to albums lists

--- src/components/AlbumListItem/AlbumListItem.js
+++ src/components/AlbumListItem/AlbumListItem.js
@@ -3,7 +3,20 @@ import { Link } from "react-router-dom";
 import { withContext } from "../../contexts/AppContext";
 
 class AlbumListItem extends Component {
+  state = {
+    photo: {}
+  };
+
+  componentDidMount() {
+    fetch(
+      `https://jsonplaceholder.typicode.com/photos?albumId=${this.props.id}`
+    )
+      .then(response => response.json())
+      .then(photos => this.setState({ photo: photos[0] }));
+  }
+
   render() {
+    const { photo } = this.state;
     const {
       title,
       id,
@@ -13,6 +26,7 @@ class AlbumListItem extends Component {
     const userData = getUserDataByUserId(userId);
     return (
       <li>
+        <img src={photo.thumbnailUrl} alt="thumbnail" />
         <Link to={`/album/${id}`}>
           <h3>{title}</h3>
         </Link>
