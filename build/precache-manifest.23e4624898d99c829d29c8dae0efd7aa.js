6767900d2d0ab040790b09eacbd3301b7103298a
jan-grasewicz <j.grasewicz@gmail.com>
2019-05-16 12:40:48 +0200

    build folder

---
+++ build/precache-manifest.23e4624898d99c829d29c8dae0efd7aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f53506903399c4bc4c055d59a13635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5118322bc5106fd086ed",
+    "url": "/static/css/main.088a44a8.chunk.css"
+  },
+  {
+    "revision": "7edb46552989456f5924",
+    "url": "/static/js/2.3d73e310.chunk.js"
+  },
+  {
+    "revision": "5118322bc5106fd086ed",
+    "url": "/static/js/main.50d9f8d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
